392ed5046975c14b8519375f48d65b9766f9b7d5
Your Name <gonzalo.vicencio@hotmail.com>
2020-11-14 01:37:44 -0300

    tercero

--- app/src/main/java/com/software/pitagora_app_201/LoginActivity.java
+++ app/src/main/java/com/software/pitagora_app_201/LoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    //modificacion 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
